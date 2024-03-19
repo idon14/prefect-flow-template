6ba0b307b24f0bc9447f80f86262329008efa521
Ido Naveh <idonaveh3@outlook.com>
2024-03-19 22:50:38 +0100

    Updated documentation

--- {{cookiecutter.flow_name}}/flows/flow.py
+++ {{cookiecutter.flow_name}}/flows/flow.py
@@ -4,18 +4,24 @@ from prefect.schedules.clocks import CronClock
 
 @task
 def task1():
+    """
+    Run task #1
+    """
     return "Result of Task 1"
 
 @task
 def task2(input):
+    """
+    Run task #2
+    """
     return f"Result of Task 2 with input {input}"
 
-cron_expression = "{{ cookiecutter.cron_expression }}"  # Use the user-defined cron expression
-schedule = Schedule(clocks=[CronClock(cron_expression)])
+cron_expression = "{{ cookiecutter.cron_expression }}"  # Use the user-defined cron expression from cookiecutter parameters
+schedule = Schedule(clocks=[CronClock(cron_expression)])  # Create a Schedule object from the cron expression
 
-with Flow("{{ cookiecutter.flow_name }}", schedule=schedule) as flow:  # Use the user-defined flow name
+with Flow("{{ cookiecutter.flow_name }}", schedule=schedule) as flow:  # Setup a Flow object and assign tasks
     t1_result = task1()
     t2_result = task2(t1_result)
 
 if __name__ == "__main__":
-    flow.register(project_name="{{ cookiecutter.project_slug }}")  # Use the user-defined project name
+    flow.register(project_name="{{ cookiecutter.project_name }}")  # Register flow with Prefect server
